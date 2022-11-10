adce0dc1c126187ec65c59733a9db194902b8c29
sneha_mandal <snehacreative2016@gmail.com>
2022-11-10 21:51:28 +0530

    config changes

--- next-frontend/next-js-dapp-frontend/hardhat.config.js
+++ next-frontend/next-js-dapp-frontend/hardhat.config.js
@@ -1,18 +1,24 @@
 
 require("@nomiclabs/hardhat-waffle")
+require("dotenv").config({path:'./.env.local'})
 
 
-
-
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 module.exports = {
 
   solidity: "0.8.10",
-  defaultNetwork:"hardhat",
+  defaultNetwork:"goerli",
   networks: {
 
   hardhat:{},
   // polygon:{},
-  // goerli:{}
+  goerli:{
+    url:process.env.GOERLI_RPC_URL,
+    accounts:[PRIVATE_KEY],
+    chainId:5,
+    
+
+  }
   },
 };
