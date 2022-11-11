fd825960364190e99c86927ef6a780485b5c426c
@Lavesh-Saluja <b521032@iiit-nh.ac.in>
2022-11-11 08:35:04 +0530

    pulled

--- next-frontend/next-js-dapp-frontend/pages/index.js
+++ next-frontend/next-js-dapp-frontend/pages/index.js
@@ -1,9 +1,16 @@
-
-
-
+import { MoralisProvider } from "react-moralis"
+import { ConnectButton } from "web3uikit"
 export default function Home() {
   return (
    
-            <></>
+            <div className="flex">
+            <div className="w-[100vw] h-[10vh] bg-black">
+              <h1 className="text-white">CROWD FUNDING</h1>
+            </div>
+              <MoralisProvider appID="">
+              <ConnectButton />
+              </MoralisProvider>
+                
+            </div>
   )
 }
