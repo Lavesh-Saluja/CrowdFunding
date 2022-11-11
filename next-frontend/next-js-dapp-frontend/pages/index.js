f2f06b94048fc7cc68edaa0ede1e4ac0fb27fef4
@Lavesh-Saluja <b521032@iiit-nh.ac.in>
2022-11-11 18:33:58 +0530

    added forms and error in abi(contractData file)

--- next-frontend/next-js-dapp-frontend/pages/index.js
+++ next-frontend/next-js-dapp-frontend/pages/index.js
@@ -1,15 +1,43 @@
 import { MoralisProvider } from "react-moralis"
-import { ConnectButton } from "web3uikit"
+import { ConnectButton,NotificationProvider} from "web3uikit";
+import React,{useState} from "react";
+import Forms from "../components/Format/Forms.jsx"
+import Tables from "../components/Format/Tables.jsx"
 export default function Home() {
+  // string title,
+  // uint requiredAmount,
+  // address indexed owner,
+  // address campaignAddress,
+  // string imgUri,
+  // uint indexed timestamp,
+  // string  indexed category
+
+const [title,setTitle]=useState();
+
+
+
+  function inputEvent(event) {
+    setTitle(event.target.value);
+    const val=event.target.value;
+  }
+ 
+
+
   return (
    
             <div className="flex">
-            <div className="w-[100vw] h-[10vh] bg-black">
+            <div className="w-[100vw] h-[10vh] bg-blue-600">
               <h1 className="text-white">CROWD FUNDING</h1>
             </div>
-              <MoralisProvider appID="">
+              <MoralisProvider initializeOnMount={false}>
               <ConnectButton />
-              </MoralisProvider>
+              
+              <NotificationProvider>
+              <Forms />
+              </NotificationProvider>
+              </MoralisProvider> 
+                <h1>{title}</h1>
+                {/* <Tables /> */}
                 
             </div>
   )
